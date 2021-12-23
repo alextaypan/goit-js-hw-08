b5d6057b5f7781fc5b71e08cb77c7db6442ecf35
Oleksandr Yakob <yakoboleksandr@gmail.com>
2021-12-23 22:08:09 +0200

    complete task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,35 @@
-// import Player from '@vimeo/player';
-
-// const player = new Player('handstick', {
-//   id: 19231868,
-//   width: 640,
-// });
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
 
-player.on('ended', function () {
-  console.log('ended the video!');
-});
+// player.on('ended', function () {
+//   console.log('ended the video!');
+// });
 
-player.on('timeupdate', function (data) {
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(throttled, 1000));
+
+function throttled(data) {
   console.log('Percentage watched: ' + data.seconds);
-});
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+// .then(function (seconds) {})
+// .catch(function (error) {
+//   switch (error.name) {
+//     case 'RangeError':
+//       // the time was less than 0 or greater than the video’s duration
+//       break;
+
+//     default:
+//       // some other error occurred
+//       break;
+//   }
+// });
