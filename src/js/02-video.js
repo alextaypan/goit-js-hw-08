821676dc8e64991b64a332d35361e4b95f1de054
Oleksandr Yakob <yakoboleksandr@gmail.com>
2021-12-23 19:48:33 +0200

    update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+// import Player from '@vimeo/player';
 
+// const player = new Player('handstick', {
+//   id: 19231868,
+//   width: 640,
+// });
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on('ended', function () {
+  console.log('ended the video!');
+});
+
+player.on('timeupdate', function (data) {
+  console.log('Percentage watched: ' + data.seconds);
+});
