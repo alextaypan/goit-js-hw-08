2fff183437bb787231cd1aecbb3d68a778433550
Oleksandr Yakob <yakoboleksandr@gmail.com>
2021-12-23 17:56:29 +0200

    complete 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li style="list-style: none">
+    <a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"/>
+    </a>
+    </li>`;
+  })
+  .join('');
+galleryRef.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
